2ba59a6e517256560eb5f8c8c8a77f62f23a976f
akira <asa.shakira340@gmail.com>
2022-10-24 13:23:48 -0700

    update lualine

--- .config/nvim/lua/akira/lualine.lua
+++ .config/nvim/lua/akira/lualine.lua
@@ -33,7 +33,7 @@ local mode = {
 
 local filetype = {
 	"filetype",
-	icons_enabled = false,
+	icons_enabled = true,
 	icon = nil,
 }
 
@@ -63,19 +63,28 @@ local spaces = function()
 end
 
 lualine.setup({
-	options = {
-		icons_enabled = true,
-		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "dashboard", "NvimTree", "Outline" },
-		always_divide_middle = true,
-	},
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
 	sections = {
 		lualine_a = { branch, diagnostics },
 		lualine_b = { mode },
 		lualine_c = {},
-		-- lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_x = { diff, spaces, "encoding", filetype },
 		lualine_y = { location },
 		lualine_z = { progress },
@@ -87,7 +96,9 @@ lualine.setup({
 		lualine_x = { "location" },
 		lualine_y = {},
 		lualine_z = {},
-	},
-	tabline = {},
-	extensions = {},
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
 })
