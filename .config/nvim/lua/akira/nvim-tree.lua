d9cd53d4cd37f63383ec9662551f2a0216d093ad
akira <asa.shakira340@gmail.com>
2023-04-12 19:08:30 +0900

    remove deprecated settings

--- .config/nvim/lua/akira/nvim-tree.lua
+++ .config/nvim/lua/akira/nvim-tree.lua
@@ -19,9 +19,6 @@ nvim_tree.setup {
   hijack_cursor = false,
   hijack_netrw = true,
   hijack_unnamed_buffer_when_opening = true, -- false
-  ignore_buffer_on_setup = false,
-  open_on_setup = false,
-  open_on_setup_file = false,
   open_on_tab = false,
   ignore_buf_on_tab_change = {},
   sort_by = "name",
@@ -133,7 +130,6 @@ nvim_tree.setup {
     update_root = true, -- false
     ignore_list = {},
   },
-  ignore_ft_on_setup = {},
   system_open = {
     cmd = "",
     args = {},
