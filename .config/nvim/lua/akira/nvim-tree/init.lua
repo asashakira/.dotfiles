26723948382655cfc2a6e6d542a1f5d45224f356
akira <asa.shakira340@gmail.com>
2023-09-06 17:42:36 +0900

    update nvim-tree

--- .config/nvim/lua/akira/nvim-tree/init.lua
+++ .config/nvim/lua/akira/nvim-tree/init.lua
@@ -11,7 +11,14 @@ local on_attach = require("akira.nvim-tree.on-attach").on_attach
 
 nvim_tree.setup {
   on_attach = on_attach,
+  view = {
+    width = table,
+  },
+  update_focused_file = {
+    enable = true,
+    update_root = true,
+  },
   filters = {
-    dotfiles = true,
+    dotfiles = false,
   },
 }
